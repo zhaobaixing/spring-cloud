908d75353533a72a2a910ec8a309a29b2f1ed978
zhaobaixing <329489689@qq.com>
2019-06-11 15:46:59 +0800

    first commit

---
+++ txpccloud-open-api/src/main/java/com/txpc/txpccloud/openapi/TxpccloudOpenApiApplication.java
@@ -0,0 +1,13 @@
+package com.txpc.txpccloud.openapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TxpccloudOpenApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TxpccloudOpenApiApplication.class, args);
+    }
+
+}
